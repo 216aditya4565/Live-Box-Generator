8a49d506db4f30d71601b0ff09b010d6d1a0d4d7
216aditya4565 <31154710+216aditya4565@users.noreply.github.com>
2020-08-01 00:26:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 var count = 0;
 
 function greater() {
-    var number1 = document.getElementById('num1').value;
-    var color = document.getElementById('color').value;
+    var number1 = document.getElementById('num1').value;  //Get the number of boxes from HTML file
+    var color = document.getElementById('color').value;   //Get the colour from HTML file
 
-    for (let i = 0; i < number1; i++) {
-        count++;
-        var btn = document.createElement("button");
-        btn.className = "btn1";
-        btn.innerHTML = "new Button " + count;
-        btn.style = "background-color:" + (color) + ";"
-        document.body.appendChild(btn);
+    //The for loop runs as many times as the input given by the user.
+    for (let i = 0; i < number1; i++) {    
+        count++; 
+        var btn = document.createElement("button");  //Creating a variable btn of button type
+        btn.className = "btn1"; 
+        btn.innerHTML = "new Button " + count; //Creating the required number of buttons as input given by user
+        btn.style = "background-color:" + (color) + ";" //Giving colour to the boxes
+        document.body.appendChild(btn);  //Inserting the button into the document
     }
 
 }
